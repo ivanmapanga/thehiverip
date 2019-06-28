69abcab145d9a9d9193e0fa5c0712ee75129fa40
Tafadzwa Muteke <muteket@gmail.com>
2019-06-28 02:24:43 +0200

    edit: changes to app.js

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const mysql = require("mysql");
-const bodyparser = require("body-parser");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.json());
 app.use(
-  bodyparser.urlencoded({
+  bodyParser.urlencoded({
     extended: false
   })
 );
-app.use(bodyparser.json());
-app.listen(8083, () => console.log("node is running on port 8083"));
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -19,32 +19,47 @@ const connection = mysql.createConnection({
   database: "projectdb"
 });
 
-app.post("/profile", function(req, res) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Authorization, Content-Type, Accept"
   );
-  var data = JSON.parse(req.body.data);
-  var name = data.name;
-  var surname = data.surname;
-  connection.connnect(function() {
-    var query =
-      "INSERT into 'profile' (name, surname) VALUES('" +
-      name +
-      "', '" +
-      surname +
-      "')";
-    connection.query(query, function(err, result, field) {
-      if (err) {
-        return res.status(500).send(err);
-      } else {
-        if (result.affectedRows > 0) {
-          res.end("success");
-        } else {
-          res.end("Try again");
-        }
-      }
-    });
-  });
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
 });
+
+module.exports = app;
+
+// app.post("/profile", function(req, res) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   var data = JSON.parse(req.body.data);
+//   var name = data.name;
+//   var surname = data.surname;
+//   connection.connnect(function() {
+//     var query =
+//       "INSERT into 'profile' (name, surname) VALUES('" +
+//       name +
+//       "', '" +
+//       surname +
+//       "')";
+//     connection.query(query, function(err, result, field) {
+//       if (err) {
+//         return res.status(500).send(err);
+//       } else {
+//         if (result.affectedRows > 0) {
+//           res.end("success");
+//         } else {
+//           res.end("Try again");
+//         }
+//       }
+//     });
+//   });
+// });
